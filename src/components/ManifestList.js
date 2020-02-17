84c81234cb4e9d48ae2dd0f8683f39bc0edb97fb
olessiap <olessia.potapova@gmail.com>
2020-02-17 10:03:17 -0300

    first UI commit

---
+++ src/components/ManifestList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class ManifestList extends Component {
+  render() {
+    return (
+      <div className="manifest-list">
+        <ul>
+          <li>Get a remote, dollar paying job</li>
+          <li>Visit mom whenever</li>
+          <li>save 5k a month</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default ManifestList
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <nav>
+        Manifest your Reality
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
