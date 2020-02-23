a6d272c949d8118fbc35b67e9483abb407b5483d
olessiap <olessia.potapova@gmail.com>
2020-02-23 13:57:34 -0300

    added useContext hooks and changed all components to be functional

--- src/components/ManifestList.js
+++ src/components/ManifestList.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
 
-export class ManifestList extends Component {
-  static contextType = ThemeContext
-  render() {
-    const { isLightTheme, light, dark } = this.context
-    const theme = isLightTheme ? light : dark
-
-    return (
-      <div className="manifest-list" style={{ background:theme.ui, color:theme.syntax }}>
-        <ul>
-          <li style={{ background:theme.bg }}>Get a remote, dollar paying job</li>
-          <li style={{ background:theme.bg }}>Visit mom whenever</li>
-          <li style={{ background:theme.bg }}>save 5k a month</li>
-        </ul>
-      </div>
-    )
-  }
+const ManifestList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext)
+  const theme = isLightTheme ? light : dark
+  return ( 
+    <div className="manifest-list" style={{ background:theme.ui, color:theme.syntax }}>
+      <ul>
+        <li style={{ background:theme.bg }}>Get a remote, dollar paying job</li>
+        <li style={{ background:theme.bg }}>Visit mom whenever</li>
+        <li style={{ background:theme.bg }}>save 5k a month</li>
+      </ul>
+    </div>
+   )
 }
-
+ 
 export default ManifestList
+
--- src/components/ToggleTheme.js
+++ src/components/ToggleTheme.js
@@ -1,14 +1,11 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
 
-export class ToggleTheme extends Component {
-  static contextType = ThemeContext
-  render() {
-    const { toggleTheme } = this.context
-    return (
-      <button onClick={toggleTheme}>Change Theme</button>
-    )
-  }
-}
+const ToggleTheme = () => {
+  const { toggleTheme } = useContext(ThemeContext)
 
+  return(
+    <button onClick={toggleTheme}>Change Theme</button>
+  )
+}
 export default ToggleTheme
