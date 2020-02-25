5c1c6131b3c27866c35b527d746142f6f8504cb5
olessiap <olessia.potapova@gmail.com>
2020-02-24 21:08:44 -0300

    added session storage for the thank yous

--- src/contexts/ThankYouContext.js
+++ src/contexts/ThankYouContext.js
@@ -1,11 +1,16 @@
-import React, { useReducer, createContext } from 'react'
+import React, { useEffect, useReducer, createContext } from 'react'
 import { thankYouReducer } from './reducers/thankYouReducer';
 
 export const ThankYouContext = createContext()
 
 const ThankYouContextProvider = (props) => {
-  const [thankYous, dispatch] = useReducer(thankYouReducer, [])
-  
+  const [thankYous, dispatch] = useReducer(thankYouReducer, [], () => {
+    const localData = localStorage.getItem('thankYous')
+    return localData ? JSON.parse(localData) : []
+  })
+  useEffect(() => {
+    localStorage.setItem('thankYous', JSON.stringify(thankYous))
+  }, [thankYous])
   return (
     <ThankYouContext.Provider value={{thankYous, dispatch}}>
       {props.children}
